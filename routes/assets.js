eca0bd3b63bd08cb8562e5d536cfebc3239edca5
DohyungKim <ehgud456456@naver.com>
2020-12-08 20:40:28 +0900

    assets

--- routes/assets.js
+++ routes/assets.js
@@ -1,26 +1,14 @@
-var express = require('express')
-var router = express.Router()
-const template = require('../lib/template.js');
-const { coinData } = require('../lib/pricing.js');
+const express = require('express')
+const router = express.Router()
+const jwt = require("jsonwebtoken");
+
 const { wallet } = require('../lib/wallet.js');
 const { authentication } = require('../lib/authentication.js');
-
-const { User } = require("../models/user");
-const { Coin } = require("../models/coin");
-const { Asset } = require("../models/asset");
-const jwt = require("jsonwebtoken");
-const { isValidObjectId } = require('mongoose');
 
 router.get("/", authentication, (req, res) => {
     jwt.verify(req.token, 'secretkey', async (err, authData) => {
         if (err) return res.sendStatus(403);
-        // const assets = await Asset.find({ user: req.user });
-        // const doc_usd = await Coin.find({ code: 'usd' });
-        // const code_usd = doc_usd[0]._id;
-        // const assetInfo_usd = await Asset.find({ user: req.user, coin: code_usd });
-        // const asset_usd = assetInfo_usd[0].quantity;
         assets = await wallet(req,res);
-        console.log(assets);
         res.send(assets);
     });
 });
--- routes/coins.js
+++ routes/coins.js
@@ -10,37 +10,16 @@ const { Coin } = require("../models/coin");
 const { Asset } = require("../models/asset");
 
 router.get("/", async (req, res) => {
-  // const coins = await Coin.find();
-  // res.send(coins);
   const prices = await coinData();
-  let datalist =
-    `<ul>
-    <li> Bitcoin (BTC) : ${prices.bitcoin} (usd) <a href='/coins/bitcoin'>거래</a> </li>
-    <li> Ethereum (ETH) : ${prices.ethereum} (usd) <a href='/coins/ethereum'>거래</a> </li>
-    <li> Ripple (XRP)  : ${prices.ripple} (usd) <a href='/coins/ripple'>거래</a> </li>
-    <li> Bitcoin Cash (BCH)  : ${prices['bitcoin-cash']} (usd) <a href='/coins/bitcoin-cash'>거래</a> </li>
-    <li> Eos (EOS)  : ${prices.eos} (usd) <a href='/coins/eos'>거래</a> </li>
-    <li> Tron (TRX)  : ${prices.tron} (usd) <a href='/coins/tron'>거래</a> </li>
-    </ul>`
-  const html = template.HTML('CoinData', datalist);
-  res.send(html);
+  res.send(prices);
 });
 
 router.get("/:coin_name", async (req, res) => {
+  const prices = await coinData();
   const wannaCoin = req.params.coin_name;
-  let form = `
-  <form action="/coins/${wannaCoin}/buy" method="post">
-    <p>wanna buy</p>
-    <p><input type="number" name="quantity" placeholder="quantity"> ${wannaCoin}</p>
-    <p><input type="submit" value="Buy"></p>
-  </form>
-  <form action="/coins/${wannaCoin}/sell" method="post">
-    <p>wanna sell</p>
-    <p><input type="number" name="quantity" placeholder="quantity"> ${wannaCoin}</p>
-    <p><input type="submit" value="Sell"></p>
-  </form>`;
-  const html = template.HTML(wannaCoin, form);
-  res.send(html);
+  if (!prices[wannaCoin]) return res.sendStatus(404);
+  const coinInfo = [wannaCoin, prices[wannaCoin]];
+  res.send(coinInfo);
 });
 
 router.post("/:coin_name/buy", authentication, async (req, res) => {
