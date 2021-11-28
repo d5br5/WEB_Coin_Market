75b0dc18335074bdfcec0588321d2991ffe28d97
DohyungKim <ehgud456456@naver.com>
2021-11-29 03:31:26 +0900

    back up revert

--- src/components/asset.js
+++ src/components/asset.js
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 
 const Button = styled.input`
-	border: none;
-	border-radius: 3px;
-	background-color: ${(props) => props.bgColor};
-	color: white;
-	text-align: center;
-	padding: 8px 0;
-	font-weight: 600;
-`;
+  border: none;
+  border-radius: 3px;
+  background-color: ${props => props.bgColor};
+  color: white;
+  text-align: center;
+  padding: 8px 0;
+  font-weight: 600;
+  
+`
 
 const colors = {
-	buy: "dodgerblue",
-	sell: "tomato",
-};
+    buy: "dodgerblue",
+    sell: "tomato"
+}
 
 export const LOGIN_KEY = "LOGIN_KEY";
 export const BUY = "BUY";
